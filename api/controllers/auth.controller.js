4ea5f6c3ceb74bef597cf915a808e6edf818300a
JainamKarania <jainamkarania05@gmail.com>
2024-03-16 11:59:23 +0530

    middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs'
-export const register = async (req,res)=>{
+import { errorhandler } from "../utils/error.js";
+export const register = async (req,res,next)=>{
     const {username , email , password } = req.body;
     if(!username || !email || !password || username==='' || email==='' || password===''){
-        return res.status(400).json({message : 'All fields are required'});
+        next(errorhandler(400,'All feilds are required'));
     }
-    const hashpass = await bcryptjs.hashSync(password,10);
+    const hashpass = bcryptjs.hashSync(password,10);
     const newUser = new User({
         username,
         email,
@@ -15,7 +16,6 @@ export const register = async (req,res)=>{
     await newUser.save();
     res.json("Registration Successfull");
     } catch(error){
-        res.status(500).json({message : error.message});
+        next(error);
     }
 }
-export default register;
